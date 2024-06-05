c27f1f13a56e5c6be1ffc985b681689f847b1100
krishnaveni <krishnaveniv90@gmail.com>
2024-06-05 23:10:30 +0200

    add filter function to filter out users

--- src/pages/Userlist/UserList.js
+++ src/pages/Userlist/UserList.js
@@ -23,11 +23,22 @@ export default function UserList()  {
        })();
     },[endIndex]);
 
+    const filterCards = event => {  
+        const value = event.target.value.toLowerCase();
+        const filteredUsers = allUsers.filter(
+          user => (`${user.name.first} ${user.name.last}`
+          .toLowerCase()
+          .includes(value.toLowerCase()))
+        );
+        setUsers(filteredUsers);
+      };
+
     return (
         <div className='user_page' >
             <div className='headings'>
             <h1>Users List</h1>
             </div>
+            <input className='search-box' placeholder='Enter name...' onInput={filterCards}/> 
            <div className='container' >
             {users.map((users, index) => (
                 <SocialCard userData={users} key={index} index={index}></SocialCard>
